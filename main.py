f0f28c7a05043688aca9f899032d4b858481c0c7
arnold <arnwolfie5@gmail.com>
2025-01-17 08:21:17 +0200

    Final commit for the project...

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pandas
+import pandas as pd
 
-url = "https://books.toscrape.com/catalogue/page-1.html"
+current_page = 1
+data = []
+proceed = True
 
-page = requests.get(url)
+while proceed:
+    print("Scraping page " + str(current_page) + "...")
+    url = "https://books.toscrape.com/catalogue/page-" + str(current_page) + ".html"
+    page = requests.get(url)
 
-print(page.text)
+    soup = BeautifulSoup(page.text, "html.parser")
 
-soup = BeautifulSoup(page.text, "html.parser")
+    if soup.title.text == "404 Not Found":
+        proceed = False
+    else:
+        all_books = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+        
+        for book in all_books:
+            item = {}
+            item["Title"] = book.find("img").attrs["alt"]
+            item["Link"] = "https://books.toscrape.com/catalogue/" + book.find("a").attrs["href"]
+            item["Price"] = book.find("p", class_="price_color").text[1:]
+            item["Stock"] = book.find("p", class_="instock availability").text.strip()
+            data.append(item)
+            
+    current_page += 1
 
-print(soup.title.text)
+df = pd.DataFrame(data)
+df.to_csv("books.csv")
+print("\n")
+print("Successfully saved to books.csv")
